c08d06c99c1ab8357433a64b700a411b600f211f
meiqi-gu <77755155+meiqi-gu@users.noreply.github.com>
2023-05-09 11:42:25 -0400

    add endpoints from a04

--- server.js
+++ server.js
@@ -63,6 +63,75 @@ const app = express()
 const port = args.port || args.p || process.env.PORT || 8080
 // Load app middleware here to serve routes, accept data requests, etc.
 //
+app.use(express.static('./public')); 
+
+app.get("/app/", (req, res, next) => {
+    res.status(200).end('200 OK'); 
+}); 
+
+// Endpoint for RPS & RPSLS with no input. 
+app.get('/app/rps/', (req, res) => {
+    const play = rps(); 
+    res.status(200).send(play); 
+}); 
+
+app.get('/app/rpsls/', (req, res) => {
+    const play = rpsls(); 
+    res.status(200).send(play); 
+}); 
+
+// For RPS URL/JSON.
+app.get('/app/rps/play/', (req, res) => {
+    const input = req.query.shot; 
+    const play = rps(input); 
+    res.send(play); 
+}); 
+
+app.use(express.json()); 
+app.use(express.urlencoded({extended: true})); 
+
+app.post('/app/rps/play/', (req, res) => {
+    const input = req.body.shot;
+    const play = rps(input); 
+    res.send(play); 
+    res.end(); 
+}); 
+
+// For RPSLS URL/JSON. 
+app.get('/app/rpsls/play/', (req, res) => {
+    const input = req.query.shot; 
+    const play = rpsls(input); 
+    res.send(play); 
+}); 
+
+app.use(express.json()); 
+app.use(express.urlencoded({extended: true})); 
+
+app.post('/app/rpsls/play/', (req, res) => {
+    const input = req.body.shot;
+    const play = rpsls(input); 
+    res.send(play); 
+    res.end(); 
+}); 
+
+// Endpoint /app/___/play/options/
+app.get('/app/rps/play/:input/', (req, res) => {
+    const play = rps(req.params.input); 
+    res.status(200).send(play); 
+});
+
+app.get('/app/rpsls/play/:input/', (req, res) => {
+    const play = rpsls(req.params.input); 
+    res.status(200).send(play); 
+})
+
+// Default API endpoint
+app.use(function(req, res) {
+    const statusCode = 404; 
+    const statusMsg = 'NOT FOUND'; 
+    res.status(statusCode).end(statusCode+' '+statusMsg); 
+}); 
+
 // Create and update access log
 // The morgan format below is the Apache Foundation combined format but with ISO8601 dates
 app.use(morgan(':remote-addr - :remote-user [:date[iso]] ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent"',
